091856ac9f740d4074735fb465e426796a01f041
Jaco Antonio <jaco42818@gmail.com>
2023-08-06 10:26:06 -0300

    Finalização do arquivo

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Quanto é 1 + 1?",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual idioma falamos?",
+    answer: "Portugues",
+    options: [
+      "Brasileiro",
+      "ingles",
+      "Portugues",
+      "Venezuelano"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Raiz de 25:",
+    answer: "As duas coisas",
+    options: [
+      "5",
+      "10",
+      "15",
+      "20"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual o dia internacional da dança?",
+    answer: "29 de abril",
+    options: [
+      "9 de dezembro",
+      "19 de junho",
+      "28 de maio",
+      "29 de abril"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual o certo:",
+    answer: "pães",
+    options: [
+      "pães",
+      "pões",
+      "pãos",
+      "não sei"
+    ]
+  },
+];
